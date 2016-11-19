4f651f09a73ae28a2d87ec8fb99978fdb8f2c5ba
Chaitra Ramachandra <chaitrapatgar@gmail.com>
2016-11-18 19:28:24 -0700

    Faculty filtering bug-fixed

--- Software-Project/personal/urls.py
+++ Software-Project/personal/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
 	
 	#Faculty Page URLs
 	url(r'^facultyhome$', views.facultyhome , name='facultyhome'),
-	url(r'^projects/$', views.projects , name='projects'),
-	url(r'^rawmatrix/$', views.rawmatrix , name='rawmatrix'),
-	url(r'^projects/(?P<pid>\d+)$', views.project, name='project' ),
+	url(r'^projects$', views.projects , name='projects'),
+	url(r'^rawmatrix$', views.rawmatrix , name='rawmatrix'),
+	#url(r'^projects/(?P<pid>\d+)$', views.project, name='project' ),
 	url(r'^addprojects$', views.addprojects , name='addprojects'),
 	
 	#Student Page URLs
